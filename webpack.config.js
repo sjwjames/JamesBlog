0bdf9e72c4592fa0b6f68eabef2ad42b8441722c
shenjianwei <shenjianwei@xindong.com>
2017-04-10 18:07:33 +0800

    init and construct

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  devtool: 'eval-source-map',
+
+  entry:  __dirname + "/app/index.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+
+  devServer: {
+    contentBase: "/public",
+    historyApiFallback: true,
+    inline: true
+  } 
+}
+ 
