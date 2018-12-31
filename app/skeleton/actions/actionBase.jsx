3f8632a89d55c01b6ea64b90c222ca7541cc4f0c
shenjianwei <shenjianwei@xindong.com>
2018-12-30 21:16:13 -0800

    rebuild

--- app/skeleton/actions/actionBase.jsx
+++ app/skeleton/actions/actionBase.jsx
@@ -1,5 +1,5 @@
 export const DEV_API_PATH = `http://localapi.sjwjames.xyz/api/v1`
-export const PRO_API_PATH = `https://api.sjwjames.xyz/api/v1`
+export const PRO_API_PATH = `http://api.sjwjames.xyz/api/v1`
 
 export default function getApiPath() {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
