5738ad74908d7680313fdd55d66b587ea0106ae0
shenjianwei <shenjianwei@xindong.com>
2019-01-06 15:53:48 -0800

    add disqus

--- app/components/post/postComponent.jsx
+++ app/components/post/postComponent.jsx
@@ -4,10 +4,18 @@ import 'react-quill/dist/quill.snow.css'
 import ReactQuill from 'react-quill'
 import Delta from 'quill-delta'
 import './post.less'
+import Disqus from 'disqus-react';
 export default class PostComponent extends Component {
     render() {
-        const {postData} = this.props
-        const postContent = new Delta((postData.data&&postData.data.content)?postData.data.content:[])
+        const { postData,id } = this.props
+        const postContent = new Delta((postData.data && postData.data.content) ? postData.data.content : [])
+        const disqusShortname = 'james-blog-1';
+        const disqusConfig = {
+            url: this.props.location,
+            identifier: this.props.id,
+            title: (postData.data && postData.data.title) ? postData.data.title : 'Loading'
+        };
+
         return (
             <div className="ui container">
                 {/* <div className="ui three wide column dividing rail">
@@ -15,8 +23,9 @@ export default class PostComponent extends Component {
                         Left Rail Content
                     </div>
                 </div> */}
-                <h3 style={{textAlign:'center'}}>{(postData.data&&postData.data.title)?postData.data.title:'Loading'}</h3>
-                <ReactQuill className="sixteen wide column post-content-box" value={postContent} readOnly={true} modules={{toolbar:false}}/>
+                <h3 style={{ textAlign: 'center' }}>{(postData.data && postData.data.title) ? postData.data.title : 'Loading'}</h3>
+                <ReactQuill className="sixteen wide column post-content-box" value={postContent} readOnly={true} modules={{ toolbar: false }} />
+                <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
             </div>
         )
     }
